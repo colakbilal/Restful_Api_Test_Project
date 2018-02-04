e2636ef96011b2a826269cc58428a339e7413f9b
colakbilal <34540799+colakbilal@users.noreply.github.com>
2018-02-04 13:25:23 +0300

    correct line alignment

--- __tests__/api/test_3_spec.js
+++ __tests__/api/test_3_spec.js
@@ -9,7 +9,7 @@ it('should find india alpha3code and then use it on another service call ', func
 		const india = response.json.RestResponse.result.find(country => country.name==='India');
 		const codeIndia = india.alpha3_code;
 		return frisby
-		          .fetch('http://services.groupkt.com/state/get/'+ codeIndia + '/all', {method: 'GET'})
+		              .fetch('http://services.groupkt.com/state/get/'+ codeIndia + '/all', {method: 'GET'})
 			      .expect('status', 200)
 			      .then(function(response){
 				      const state = response.json.RestResponse.result.find(item => item.id===65);
